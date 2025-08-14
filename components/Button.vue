3813b2cebad1004807c5f27336e8a49a66263a6e
iliyasold <iliyasold@gmail.com>
2025-08-14 17:34:10 +0300

    My HW2 is ready

---
+++ components/Button.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+  .button {
+    width: 335px;
+    height: 68px;
+    padding: 8px 16px;
+    border-radius: 35px;
+    font-family: var(--font), serif;
+    font-size: 24px;
+    font-weight: 400;
+    background: var(--bg-color-btn);
+    color: var(--color-btn);
+  }
+
+  .button:hover {
+    opacity: 0.8;
+  }
+</style>
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Button from "../components/Button.vue";
+</script>
+
+<template>
+  <main>
+    <Button>Начать игру</Button>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
